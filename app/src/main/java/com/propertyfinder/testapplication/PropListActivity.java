0e833445e5be28e0442dd3f519008652a6c12598
Damir Mailybayev <damir.mailybayev@emaratech.ae>
2018-02-16 11:17:05 +0400

    first commit

---
+++ app/src/main/java/com/propertyfinder/testapplication/PropListActivity.java
@@ -0,0 +1,13 @@
+package com.propertyfinder.testapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PropListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prop_list);
+    }
+}
